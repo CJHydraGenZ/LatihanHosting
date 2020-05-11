629263f3be4a647f39864cf9029022b911e0c6d5
CJHydraGenZ <cahyasaga321@gmail.com>
2020-05-11 16:03:21 +0800

    Updates

---
+++ precache-manifest.ecc6c5a75c383e329a320e9da76bf8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c79ebed262d11ba8f7dfe1c8f93e5c0",
+    "url": "/LatihanHosting/index.html"
+  },
+  {
+    "revision": "a442a7e95568383cc373",
+    "url": "/LatihanHosting/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cb02aca40cce19f767a3",
+    "url": "/LatihanHosting/static/js/2.af540e77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LatihanHosting/static/js/2.af540e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a442a7e95568383cc373",
+    "url": "/LatihanHosting/static/js/main.517d3134.chunk.js"
+  },
+  {
+    "revision": "a8fd76c0b284a4eb37c4",
+    "url": "/LatihanHosting/static/js/runtime-main.ce567bf3.js"
+  }
+]);
